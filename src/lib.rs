5bc44ace3b64e14773db80712bdb8d31f5e7eaa0
Billy J Rhoades II <me@brod.es>
2020-11-08 11:16:08 -0800

    Clippy lint.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
+#![allow(clippy::not_unsafe_ptr_arg_deref)]
 pub mod errors;
-
 #[macro_use]
 extern crate redis_module;
 
@@ -26,7 +26,7 @@ fn load_module(ctx: &Context, args: Vec<String>) -> Result<String, Error> {
         .skip(1)
         .enumerate()
         .partition(|(i, _)| *i == 0);
-    let bytes = bytes.pop().ok_or(Error::wrong_arity())?.1;
+    let bytes = bytes.pop().ok_or_else(Error::wrong_arity)?.1;
     let modargs = modargs.into_iter().map(|(_, e)| e).collect::<Vec<_>>();
 
     let bytes = base64::decode(bytes)?;
@@ -34,10 +34,12 @@ fn load_module(ctx: &Context, args: Vec<String>) -> Result<String, Error> {
 
     // This error case should never happen since we control the temp file name, right?
     // Just handling it instead of a redis-crashing panic.
-    let pathstr = file.path().to_str().ok_or(format_err!(
-        "failed to convert temp module \"{:?}\" unicode string",
-        file.path().as_os_str()
-    ))?;
+    let pathstr = file.path().to_str().ok_or_else(|| {
+        format_err!(
+            "failed to convert temp module \"{:?}\" unicode string",
+            file.path().as_os_str()
+        )
+    })?;
     let loadargs = vec!["LOAD", pathstr]
         .into_iter()
         .chain(modargs.iter().map(String::as_str))
@@ -61,6 +63,19 @@ fn dispatch(ctx: &Context, args: Vec<String>) -> RedisResult {
         .map_err(RedisError::from)
 }
 
+fn write_bytes_to_path(bytes: &[u8]) -> Result<NamedTempFile, Error> {
+    let mut file = Builder::new()
+        .prefix("redis-loadmodbytes-")
+        .suffix(".so")
+        .rand_bytes(5)
+        .tempfile()?;
+    file.write_all(&bytes)?;
+
+    // redis expects +x
+    set_permissions(file.path(), std::fs::Permissions::from_mode(0o755))?;
+    Ok(file)
+}
+
 redis_module! {
     name: "LOADMODBYTES",
     version: 1,
@@ -68,17 +83,4 @@ redis_module! {
     commands: [
         ["LOADMODBYTES", dispatch, "", 0, 0, 0],
     ],
-}
-
-fn write_bytes_to_path(bytes: &[u8]) -> Result<NamedTempFile, Error> {
-    let mut file = Builder::new()
-        .prefix("redis-loadmodbytes-")
-        .suffix(".so")
-        .rand_bytes(5)
-        .tempfile()?;
-    file.write(&bytes)?;
-
-    // redis expects +x
-    set_permissions(file.path(), std::fs::Permissions::from_mode(0o755))?;
-    Ok(file)
 }
