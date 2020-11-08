bcdaac71642dcb7d7b38cca3e1209bca24cea85a
Billy J Rhoades II <me@brod.es>
2020-11-08 08:26:43 -0800

    Final libloader-based draft which checks dylibs.

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,7 @@ extern crate dlopen_derive;
 use dlopen::wrapper::{Container, WrapperApi};
 use failure::{format_err, Error as FailError};
 use std::io::{self, Write};
-use tempfile::NamedTempFile;
+use tempfile::Builder;
 
 use redis_module::{Context, RedisError, RedisResult, RedisValue};
 
@@ -59,35 +59,47 @@ impl Error {
 // <command> <module name> <module version #> <module bytes> <...module config>
 // 1.
 // 2.
-fn load_module(args: Vec<String>) -> Result<String, Error> {
-    println!("args: {:?}", args);
+fn load_module(ctx: &Context, args: Vec<String>) -> Result<String, Error> {
     let name = args.get(0).ok_or(Error::from(RedisError::WrongArity))?;
     let version = args.get(1).ok_or(Error::from(RedisError::WrongArity))?;
     let bytes = args.get(2).ok_or(Error::from(RedisError::WrongArity))?;
 
-    let mut file = NamedTempFile::new()?;
+    let mut file = Builder::new()
+        .prefix("redis-module-shim")
+        .suffix("so")
+        .rand_bytes(5)
+        .tempfile()?;
 
     println!("b64 len: {}", bytes.len());
+
     let bytes = base64::decode(bytes)?;
     println!("file bytes: {}", bytes.len());
 
     file.write(&bytes)?;
-    let path = file.into_temp_path();
+    let (file, path) = file.keep()?;
+    std::fs::set_permissions(&path, std::fs::Permissions::from_mode(0o755))?;
 
-    let module: Container<Module> = unsafe { Container::load(path) }.map_err(|e| {
+    let pathstr = path.to_str().unwrap();
+
+    use std::os::unix::fs::PermissionsExt;
+
+    let module: Container<Module> = unsafe { Container::load(pathstr) }.map_err(|e| {
         format_err!(
             "provided module loaded, but it does not appear to be a shimmable redis module:\n\n{}",
             e
         )
     })?;
 
+    ctx.call("MODULE", &["LOAD", pathstr])
+        .map_err(|e| format_err!("failed on load module call: {}", e))?;
+
     Ok(format!("loaded {} {}", module.name(), module.version()))
 }
 
-fn dispatch(_: &Context, args: Vec<String>) -> RedisResult {
+fn dispatch(ctx: &Context, args: Vec<String>) -> RedisResult {
     let args = args.into_iter().skip(1).collect::<Vec<String>>();
 
-    match load_module(args) {
+    match load_module(ctx, args) {
         Ok(v) => RedisResult::Ok(RedisValue::SimpleString(v)),
         Err(e) => RedisResult::Err(e.into()),
     }
